ba548c4c28c120e2821ee0b771214c4c80039bcf
Manjunath <itzmemanju16@gmail.com>
2019-09-24 11:24:55 +0530

    level order traversal with direction

---
+++ Data-Structure/Trees/levelOrderWithDirection.js
@@ -0,0 +1,72 @@
+'use strict';
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.leftNode = this.rightNode = null;
+    }
+}
+function levelOrderTraversalWithDirection(root) {
+    if (root == null) return;
+    let stack = [];
+    stack.push(root);
+    let queue = [];
+    let count = 1;
+    while (stack.length != 0) {
+        let levelElements = '';
+        while (stack.length != 0) {
+            let temp = stack.pop();
+            if (count == 1) {
+                if (temp.leftNode) queue.push(temp.leftNode);
+                if (temp.rightNode) queue.push(temp.rightNode);
+            } else if (count == 2) {
+                if (temp.rightNode) queue.push(temp.rightNode);
+                if (temp.leftNode) queue.push(temp.leftNode);
+            }
+            levelElements = levelElements + temp.data + ' ';
+        }
+        console.log(levelElements);
+        levelElements = '';
+        while (queue.length != 0) {
+            let temp = queue.shift();
+            if (count == 1) {
+                if (temp.leftNode) stack.push(temp.leftNode);
+                if (temp.rightNode) stack.push(temp.rightNode);
+            } else {
+                if (temp.rightNode) stack.push(temp.rightNode);
+                if (temp.leftNode) stack.push(temp.leftNode);
+            }
+            levelElements = levelElements + temp.data + ' ';
+        }
+        console.log(levelElements);
+        if (count == 2) count = 0;
+        count++;
+    }
+}
+//level - 1
+let tree = new Node(1);
+
+// level - 2
+tree.leftNode = new Node(2);
+tree.rightNode = new Node(3);
+
+// level -3
+tree.leftNode.leftNode = new Node(4);
+tree.leftNode.rightNode = new Node(5);
+
+tree.rightNode.leftNode = new Node(6);
+tree.rightNode.rightNode = new Node(7);
+
+// level -4
+tree.leftNode.leftNode.leftNode = new Node(8);
+tree.leftNode.leftNode.rightNode = new Node(9);
+
+tree.leftNode.rightNode.leftNode = new Node(10);
+tree.leftNode.rightNode.rightNode = new Node(11);
+
+tree.rightNode.leftNode.leftNode = new Node(12);
+tree.rightNode.leftNode.rightNode = new Node(13);
+
+tree.rightNode.rightNode.leftNode = new Node(14);
+tree.rightNode.rightNode.rightNode = new Node(15);
+
+levelOrderTraversalWithDirection(tree);
