473af78eb797cd019bd4acce0002c57ef116fcef
Manjunath <itzmemanju16@gmail.com>
2019-12-01 09:48:47 +0530

    dfs algorithm added

---
+++ Data-Structure/Graph/dfs.js
@@ -0,0 +1,42 @@
+'use strict';
+class Graph {
+    constructor(vertex) {
+        this.listArray = new Array(vertex + 1);
+        this.visitedArr = new Array(vertex + 1);
+        for (let index = 0; index <= vertex; index++) {
+            this.listArray[index] = [];
+            this.visitedArr[index] = 0;
+        }
+    }
+    addEdge(v, u) {
+        this.listArray[v].push(u);
+    }
+
+    dfs(startVertex) {
+        console.log(startVertex);
+        this.visitedArr[startVertex] = 1;
+        let tList = this.listArray[startVertex];
+        for (let index = 0; index < tList.length; index++) {
+            if (!this.visitedArr[tList[index]]) this.dfs(tList[index]);
+        }
+    }
+}
+
+let g1 = new Graph(7);
+g1.addEdge(1, 2);
+g1.addEdge(1, 3);
+g1.addEdge(1, 5);
+
+g1.addEdge(2, 1);
+g1.addEdge(2, 4);
+g1.addEdge(2, 7);
+
+g1.addEdge(3, 1);
+g1.addEdge(3, 6);
+
+g1.addEdge(5, 1);
+g1.addEdge(5, 6);
+
+g1.addEdge(6, 3);
+g1.addEdge(6, 5);
+g1.dfs(1);
